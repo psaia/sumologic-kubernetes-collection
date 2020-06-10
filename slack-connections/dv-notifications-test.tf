7dbc7a9cce126c47465dad0d69ccc8492dc21bc9
Malcolm Jones <malcolm.jones@nytimes.com>
2020-06-09 18:38:32 -0700

    add dv test

---
+++ slack-connections/dv-notifications-test.tf
@@ -0,0 +1,6 @@
+module "dv-notifications-test" {
+  source = "../modules/slack-connection"
+
+  name = "dv-notifications-test"
+  webhook = "https://hooks.slack.com/services/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"
+}
