f33aedaba27e2884a609f167fc5f0588588a837f
Marcin Suterski <marcin.suterski@nytimes.com>
2020-07-07 13:35:29 -0400

    Update how sourceCategory value is generated

--- modules/collector/main.tf
+++ modules/collector/main.tf
@@ -21,6 +21,7 @@ resource "sumologic_http_source" "sources" {
 module "collector_role" {
   source = "../role"
 
-  name          = var.name
-  search_filter = join(" OR ", concat([for src in sumologic_http_source.sources : "_source=${src.name}"], ["_sourceCategory=${src.category}"]))
+  name = local.name
+
+  search_filter = join(" OR ", [for src in sumologic_http_source.sources : "_source=${src.name} OR _sourceCategory=${src.category}"])
 }
