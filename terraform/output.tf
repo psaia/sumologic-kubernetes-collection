fa0288a1136e4c18437abffd86df0d1fb4152e2d
Lincoln Sward <lincoln.a.sward@gmail.com>
2020-07-22 17:36:26 -0400

    hopefully this is the final format fix

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,76 +1,76 @@
 # dv-test
 output "dv-test-endpoints" {
-  value = module.dv-test.http_endpoints
+  value       = module.dv-test.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the dv-test collector"
 }
 
 output "dv-test-source-category" {
-  value = module.dv-test.source_category
+  value       = module.dv-test.source_category
   description = "HTTP endpoints and sourceCategory created for the dv-test collector"
 }
 
 # nytimes-spg-petstore
 output "nytimes-spg-petstore-endpoints" {
-  value = module.nytimes-spg-petstore.http_endpoints
+  value       = module.nytimes-spg-petstore.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the nytimes-spg-petstore collector"
 }
 
 output "nytimes-spg-petstore-source-category" {
-  value = module.nytimes-spg-petstore.source_category
+  value       = module.nytimes-spg-petstore.source_category
   description = "HTTP endpoints and sourceCategory created for the nytimes-spg-petstore collector"
 }
 
 # nytimes-search-add-es
 output "nytimes-search-add-es-endpoints" {
-  value = module.nytimes-search-add-es.http_endpoints
+  value       = module.nytimes-search-add-es.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the nytimes-search-add-es collector"
 }
 
 output "nytimes-search-add-es-source-category" {
-  value = module.nytimes-search-add-es.source_category
+  value       = module.nytimes-search-add-es.source_category
   description = "HTTP endpoints and sourceCategory created for the nytimes-search-add-es collector"
 }
 
 # nytimes-search-omnibus-log2es
 output "nytimes-search-omnibus-log2es-endpoints" {
-  value = module.nytimes-search-omnibus-log2es.http_endpoints
+  value       = module.nytimes-search-omnibus-log2es.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the nytimes-search-omnibus-log2es collector"
 }
 
 output "nytimes-search-omnibus-log2es-source-category" {
-  value = module.nytimes-search-omnibus-log2es.source_category
+  value       = module.nytimes-search-omnibus-log2es.source_category
   description = "HTTP endpoints and sourceCategory created for the nytimes-search-omnibus-log2es collector"
 }
 
 # nytimes-spg-productservice
 output "nytimes-spg-productservice-endpoints" {
-  value = module.nytimes-spg-productservice.http_endpoints
+  value       = module.nytimes-spg-productservice.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the nytimes-spg-productservice collector"
 }
 
 output "nytimes-spg-productservice-source-category" {
-  value = module.nytimes-spg-productservice.source_category
+  value       = module.nytimes-spg-productservice.source_category
   description = "HTTP endpoints and sourceCategory created for the nytimes-spg-productservice collector"
 }
 
 # nytm-pubsys-output-scripts
 output "nytm-pubsys-output-scripts-endpoints" {
-  value = module.nytm-pubsys-output-scripts.http_endpoints
+  value       = module.nytm-pubsys-output-scripts.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the nytm-pubsys-output-scripts collector"
 }
 
 output "nytm-pubsys-output-scripts-source-category" {
-  value = module.nytm-pubsys-output-scripts.source_category
+  value       = module.nytm-pubsys-output-scripts.source_category
   description = "HTTP endpoints and sourceCategory created for the nytm-pubsys-output-scripts collector"
 }
 
 # nytm-pubsys-newsgate-ConfigBase1
 output "nytm-pubsys-newsgate-ConfigBase1-endpoints" {
-  value = module.nytm-pubsys-newsgate-ConfigBase1.http_endpoints
+  value       = module.nytm-pubsys-newsgate-ConfigBase1.http_endpoints
   description = "HTTP endpoints and sourceCategory created for the nytm-pubsys-newsgate-ConfigBase1 collector"
 }
 
 output "nytm-pubsys-newsgate-ConfigBase1-source-category" {
-  value = module.nytm-pubsys-newsgate-ConfigBase1.source_category
+  value       = module.nytm-pubsys-newsgate-ConfigBase1.source_category
   description = "HTTP endpoints and sourceCategory created for the nytm-pubsys-newsgate-ConfigBase1 collector"
 }
