c2dd0b12c465f9dbff9dc30c86119efd9446a683
Lincoln Sward <lincoln.a.sward@gmail.com>
2020-07-28 12:46:46 -0400

    forgot to revert the check I was doing for the EOF stuff

--- tools/get-existing-sumologic-resources/create-tf-config-file.py
+++ tools/get-existing-sumologic-resources/create-tf-config-file.py
@@ -169,7 +169,7 @@ def write_resource_to_file(resource_type: str, resource: dict, resource_name: st
                         val.reverse()
                         fp.write(f"""    {key} = {val}\n""".replace("'", '"'))
                     else:
-                        if val.find('\n') != -1:
+                        if (val.find(" or ") != -1 or val.find(" OR ") != -1 ) or val.find('\n') != -1:
                             # End-User workstation decided to use double quotes around some the items in the role filter
                             # They are the only team. I would just suggest changing them manually and start a
                             # conversation with that team about why that was necessary.
