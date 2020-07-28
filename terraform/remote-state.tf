0054c89e249ea41de4df086fccb3cedc49cfe2d3
Malcolm Jones <17786500+mlclmj@users.noreply.github.com>
2020-07-28 10:51:39 -0700

    Make it hard to use the prod state on accident (#22)
    * make local state the default
    
    * remove trailing spaces
    
    * re-enable apply
    
    * move all remote-state declarations into the .drone.yml
    
    * remove renaming step
    
    * remove newline
    
    Co-authored-by: Marcin Suterski <msuterski@users.noreply.github.com>

--- terraform/remote-state.tf
+++ terraform/remote-state.tf
@@ -1,6 +1,7 @@
+# Be careful making changes in this file -- you can accidentally find yourself
+# working with the live production state file.
+# Please reach out in #delivery-engineering if you'd like help working with this.
+
 terraform {
-  backend "gcs" {
-    bucket = "remote-state"
-    prefix = "nytimes/dv-sumologic"
-  }
+  backend "gcs" {}
 }
