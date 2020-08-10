8cdda2d1b44d2b23f46e56a91c432f3ab2028be1
Malcolm Jones <17786500+mlclmj@users.noreply.github.com>
2020-08-10 10:50:02 -0700

    Add Default Roles for Azure App AD Groups (#44)
    * add default roles
    
    * add drone vars
    
    * update descrip
    
    * fix filter

---
+++ terraform/default-roles.tf
@@ -0,0 +1,5 @@
+resource "sumologic_role" "default-role" {
+  name             = var.default_role
+  description      = "Default AD Group role for Sumo Logic"
+  filter_predicate = "_sourceCategory=/dev/null"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "default_role" {
+  type        = string
+  description = "The default AD group that users are added to for Azure SAML integration."
+}
