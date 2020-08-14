7d8de529af41f36e7141418c053838b6b4309acc
Marcin Suterski <msuterski@users.noreply.github.com>
2020-08-14 12:56:50 -0400

    Disable resources in AD until we cna fix LDAP connections to AD (#53)

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -9,10 +9,11 @@ resource "sumologic_role" "sumo-role" {
   capabilities     = var.capabilities
 }
 
-# Generate AD group to match the Sumo Logic role
-resource "ad_group_to_ou" "ad-group" {
-  ou_distinguished_name = "OU=SumoLogic RBAC,OU=2FA Objects,OU=NYTMG,DC=ent,DC=nytint,DC=com"
-  group_name            = local.name
-  description           = "AD Group for Sumo Logic RBAC group ${local.name}"
-  auto_gid              = true
-}
+# Commenting this out until we can fix LDAP connections to AD
+# # Generate AD group to match the Sumo Logic role
+# resource "ad_group_to_ou" "ad-group" {
+#   ou_distinguished_name = "OU=SumoLogic RBAC,OU=2FA Objects,OU=NYTMG,DC=ent,DC=nytint,DC=com"
+#   group_name            = local.name
+#   description           = "AD Group for Sumo Logic RBAC group ${local.name}"
+#   auto_gid              = true
+# }
