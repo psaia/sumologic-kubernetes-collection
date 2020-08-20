cd4ada6cb618957d5847754511e4cfa4048b87f0
Mel Cone <mel.cone@nytimes.com>
2020-08-20 16:46:31 -0400

    Include changes to roles that were made in ui (#64)
    * Include changes to roles that were made in ui
    
    * Revoke s3 forwarding permission

--- terraform/WindowsDC-role.tf
+++ terraform/WindowsDC-role.tf
@@ -1,7 +1,7 @@
 resource "sumologic_role" "WindowsDC" {
   name             = "${var.role_prefix[var.env]}-WindowsDC"
   description      = "Windows DC"
-  filter_predicate = "_sourceCategory=${sumologic_http_source.https-collector_dev_windows-dc.category} OR _sourceCategory=virtualization-esx"
+  filter_predicate = "_sourceCategory=dns-daemon OR _sourceCategory=${sumologic_http_source.https-collector_dev_windows-dc.category} OR _sourceCategory=virtualization-esx"
   capabilities     = ["viewScheduledViews", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "changeDataAccessLevel"]
 }
 
