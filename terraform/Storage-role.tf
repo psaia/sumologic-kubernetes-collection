2c86333d62082111fc38e4b4631a4e772ec32485
Malcolm Jones <17786500+mlclmj@users.noreply.github.com>
2020-08-21 13:53:05 -0700

    Update Storage-role.tf (#67)

--- terraform/Storage-role.tf
+++ terraform/Storage-role.tf
@@ -1,7 +1,7 @@
 resource "sumologic_role" "Storage" {
   name             = "${var.role_prefix[var.env]}-Storage"
   description      = "Storage Team"
-  filter_predicate = "_sourceCategory=${sumologic_http_source.FTPS_ftps-dev-syslog.name} OR _sourceCategory=${sumologic_http_source.FTPS_ftps-stg-syslog.category} OR _sourceCategory=${sumologic_http_source.FTPS_ftps-prd-syslog.category} OR _sourceCategory=AWS_EAGLE"
+  filter_predicate = "_sourceCategory=${sumologic_http_source.FTPS_ftps-dev-syslog.name} OR _sourceCategory=${sumologic_http_source.FTPS_ftps-stg-syslog.category} OR _sourceCategory=${sumologic_http_source.FTPS_ftps-prd-syslog.category} OR _sourceCategory=AWS_EAGLE OR _sourceCategory=${module.nytimes-infra-ftp-services.search_filter}"
   capabilities     = ["viewScheduledViews", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "changeDataAccessLevel"]
 }
 
