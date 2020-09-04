075cb62aae0f1435b32b258a67975172cb2d94e2
Malcolm Jones <17786500+mlclmj@users.noreply.github.com>
2020-09-04 08:35:41 -0700

    Remove AD Group for Guest (#80)
    * Update Guest-role.tf
    
    * Update Guest-role.tf
    
    Co-authored-by: Marcin Suterski <msuterski@users.noreply.github.com>

--- terraform/Guest-role.tf
+++ terraform/Guest-role.tf
@@ -1,12 +1,8 @@
+# The guest role is a special one which users are added to by default when their accounts are first provisioned.
+# This is not one that we want to manage through AD and it's scope should be limited.
 resource "sumologic_role" "Guest" {
   name             = "${var.role_prefix[var.env]}-Guest"
   description      = "Default Guest User"
   filter_predicate = "_sourceCategory=guest"
   capabilities     = ["viewScheduledViews", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "changeDataAccessLevel"]
-}
-
-resource "ad_group_to_ou" "ad-group-Guest" {
-  ou_distinguished_name = var.ad_oudn
-  group_name            = sumologic_role.Guest.name
-  description           = "AD Group for Sumo Logic RBAC group ${sumologic_role.Guest.name}"
 }
