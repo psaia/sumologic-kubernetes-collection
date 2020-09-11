4e276ea0d0f2ae9ed0ba05243ea7b257eb2001ab
Mel Cone <mel.cone@nytimes.com>
2020-09-11 12:07:06 -0400

    Add int-fallback role (#86)

---
+++ terraform/int-fallback-role.tf
@@ -0,0 +1,18 @@
+/*resource "sumologic_role" "int-fallback" {
+    name = "${var.role_prefix[var.env]}-int-fallback"
+    filter_predicate = "(_source=${sumologic_http_source.int-fallback-dev_logs.name} AND _collector=${sumologic_collector.int-fallback-dev.name}) OR (_source=logs AND _collector=int-fallback-stg)"
+    capabilities = ["viewFields", "viewFieldExtraction", "viewPartitions", "viewScheduledViews"]
+}*/
+
+/*resource "ad_group_to_ou" "ad-group-int-fallback" {
+  ou_distinguished_name = var.ad_oudn
+  group_name            = sumologic_role.int-fallback.name
+  description           = "AD Group for Sumo Logic RBAC group ${sumologic_role.int-fallback.name}"
+}*/
+
+resource "ad_group_to_ou" "ad-group-int-fallback" {
+  ou_distinguished_name = var.ad_oudn
+  group_name            = "${var.role_prefix[var.env]}-int-fallback"
+  description           = "AD Group for Sumo Logic RBAC group ${var.role_prefix[var.env]}-int-fallback"
+}
+
