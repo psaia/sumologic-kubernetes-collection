8b98e055340b13ae63ac63f6427491c17b08bf8c
Malcolm Jones <17786500+mlclmj@users.noreply.github.com>
2020-09-17 16:59:35 -0400

    Fix default role for admins (#92)
    * Update default-roles.tf
    
    * Update default-roles.tf

--- terraform/default-roles.tf
+++ terraform/default-roles.tf
@@ -1,5 +1,4 @@
 resource "sumologic_role" "default-role" {
-  name             = var.default_role
-  description      = "Default AD Group role for Sumo Logic"
-  filter_predicate = "_sourceCategory=/dev/null"
+  name        = var.default_role
+  description = "Default AD Group role for Sumo Logic"
 }
