753730ebf1b3003700c578afcc58d5e890aadd1d
Malcolm Jones <17786500+mlclmj@users.noreply.github.com>
2020-09-22 12:03:07 -0400

    remove 'manageIndexes' from samizdat role (#102)

--- terraform/Samizdat-role.tf
+++ terraform/Samizdat-role.tf
@@ -2,7 +2,7 @@ resource "sumologic_role" "Samizdat" {
   name             = "${var.role_prefix[var.env]}-Samizdat"
   description      = "Samizdat Application Logs"
   filter_predicate = "(_sourceCategory=linux-* AND _sourceName=*/samizdat-*/*.log) OR _sourceCategory=${sumologic_http_source.GKE_Samizdat_Dev.category} OR _sourceCategory=webandmobile-samizdat-iad OR _sourceCategory=samizdat-graphql-elb-prd-iad1 OR _sourceCategory=samizdat-graphql-elb-prd-pdx1 OR _sourceCategory=${sumologic_http_source.HTTP_webprd-www.category} OR _sourceCategory=${sumologic_http_source.HTTP_fastly-www_prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_samizdat-fluentd.category}"
-  capabilities     = ["viewScheduledViews", "metricsExtraction", "manageFieldExtractionRules", "manageMonitors", "viewPartitions", "manageIndexes", "manageScheduledViews", "viewFields", "viewFieldExtraction", "manageFields"]
+  capabilities     = ["viewScheduledViews", "metricsExtraction", "manageFieldExtractionRules", "manageMonitors", "viewPartitions", "manageScheduledViews", "viewFields", "viewFieldExtraction", "manageFields"]
 }
 
 resource "ad_group_to_ou" "ad-group-Samizdat" {
