683cadc6a3c461f4da0d53069ca06bb3f6002ced
Marcin Suterski <msuterski@users.noreply.github.com>
2020-10-29 15:36:30 -0400

    Add Offers role (#145)

--- terraform/teams.tf
+++ terraform/teams.tf
@@ -4,3 +4,13 @@ module "dv" {
   name = "dv"
   env  = var.env
 }
+
+module "offers" {
+  source = "../modules/role"
+
+  name          = "offers"
+  search_filter = "_sourceCategory=${sumologic_http_source.HTTP_fastly-mktg_prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_fastly-mktg_stg.category}"
+  capabilities  = ["viewScheduledViews", "manageFieldExtractionRules", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "createAccessKeys", "viewCollectors", "manageFields"]
+  group_manager = "CN=Thiaw\\, Lamine,OU=Users,OU=NYHQ,OU=NYTMG,DC=ent,DC=nytint,DC=com"
+  env           = var.env
+}
