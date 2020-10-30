a81011d0cb19290a61327e38d79249dc47656f9b
Misha Avrekh <32136650+mavrekh-nyt@users.noreply.github.com>
2020-10-30 14:40:11 -0400

    Added fastly-mktg-purge_stg.category and fastly-mktg-purge_prd.category to the search filter for the Offers team (#147)

--- terraform/teams.tf
+++ terraform/teams.tf
@@ -9,7 +9,7 @@ module "offers" {
   source = "../modules/role"
 
   name          = "offers"
-  search_filter = "_sourceCategory=${sumologic_http_source.HTTP_fastly-mktg_prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_fastly-mktg_stg.category}"
+  search_filter = "_sourceCategory=${sumologic_http_source.HTTP_fastly-mktg_prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_fastly-mktg_stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_fastly-mktg-purge_prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_fastly-mktg-purge_stg.category}"
   capabilities  = ["viewScheduledViews", "manageFieldExtractionRules", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "createAccessKeys", "viewCollectors", "manageFields"]
   group_manager = "CN=Thiaw\\, Lamine,OU=Users,OU=NYHQ,OU=NYTMG,DC=ent,DC=nytint,DC=com"
   env           = var.env
