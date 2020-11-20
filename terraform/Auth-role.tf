1a29d06b4f8704e3aa59879bed8147d5e40ba529
Lincoln Sward <lincoln.a.sward@gmail.com>
2020-11-20 09:29:17 -0500

    adding new GCP integration source category to role and team manager t… (#157)
    * adding new GCP integration source category to role and team manager to OU
    
    * fixed multiline string error

--- terraform/Auth-role.tf
+++ terraform/Auth-role.tf
@@ -1,7 +1,7 @@
 resource "sumologic_role" "Auth" {
   name             = "${var.role_prefix[var.env]}-Auth"
   description      = "Auth Team"
-  filter_predicate = "_sourceCategory=webandmobile-auth OR _sourceName=*auth-*/syslog.log* OR _sourceName=*auth-*/kern.log* OR _sourceName=*auth-*/daemon.log* OR _sourceCategory=webandmobile-seg OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire_Staging.category}"
+  filter_predicate = "_sourceCategory=webandmobile-auth OR _sourceName=*auth-*/syslog.log* OR _sourceName=*auth-*/kern.log* OR _sourceName=*auth-*/daemon.log* OR _sourceCategory=webandmobile-seg OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire_Staging.category} OR _sourceCategory=nyt-kraken-prd"
   capabilities     = ["viewScheduledViews", "metricsExtraction", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "createAccessKeys", "changeDataAccessLevel"]
 }
 
@@ -9,6 +9,7 @@ resource "ad_group_to_ou" "ad-group-Auth" {
   ou_distinguished_name = var.ad_oudn
   group_name            = sumologic_role.Auth.name
   description           = "AD Group for Sumo Logic RBAC group ${sumologic_role.Auth.name}"
+  group_manager         = "CN=Chu\\, Jethro,OU=Users,OU=NYHQ,OU=NYTMG,DC=ent,DC=nyint,DC=com"
   lifecycle {
     prevent_destroy = true
   }
