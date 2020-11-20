dd68d7d49e430ccaee832f1641108180bf1b4f5f
Mel Cone <7332210+melmaliacone@users.noreply.github.com>
2020-11-20 12:31:07 -0500

    Update Auth-role.tf (#158)
    Fix typo

--- terraform/Auth-role.tf
+++ terraform/Auth-role.tf
@@ -9,7 +9,7 @@ resource "ad_group_to_ou" "ad-group-Auth" {
   ou_distinguished_name = var.ad_oudn
   group_name            = sumologic_role.Auth.name
   description           = "AD Group for Sumo Logic RBAC group ${sumologic_role.Auth.name}"
-  group_manager         = "CN=Chu\\, Jethro,OU=Users,OU=NYHQ,OU=NYTMG,DC=ent,DC=nyint,DC=com"
+  group_manager         = "CN=Chu\\, Jethro,OU=Users,OU=NYHQ,OU=NYTMG,DC=ent,DC=nytint,DC=com"
   lifecycle {
     prevent_destroy = true
   }
