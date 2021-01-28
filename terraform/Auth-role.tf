2abdb2f22721074315980fa74a5fd2ef8553f3d8
Mel Cone <7332210+melmaliacone@users.noreply.github.com>
2021-01-28 18:33:55 -0500

    Add manage collectors capability to auth role (#184)
    * Add manage collectors capability
    
    * Update filter_predicate to include k8s collector

--- terraform/Auth-role.tf
+++ terraform/Auth-role.tf
@@ -1,8 +1,8 @@
 resource "sumologic_role" "Auth" {
   name             = "${var.role_prefix[var.env]}-Auth"
   description      = "Auth Team"
-  filter_predicate = "_sourceCategory=webandmobile-auth OR _sourceName=*auth-*/syslog.log* OR _sourceName=*auth-*/kern.log* OR _sourceName=*auth-*/daemon.log* OR _sourceCategory=webandmobile-seg OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire_Staging.category} OR _sourceCategory=nyt-kraken-prd"
-  capabilities     = ["viewScheduledViews", "metricsExtraction", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "createAccessKeys", "changeDataAccessLevel"]
+  filter_predicate = "_sourceCategory=webandmobile-auth OR _sourceName=*auth-*/syslog.log* OR _sourceName=*auth-*/kern.log* OR _sourceName=*auth-*/daemon.log* OR _sourceCategory=webandmobile-seg OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_myaccount-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-prd.category} OR _sourceCategory=${sumologic_http_source.HTTP_homedelivery-stg.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire.category} OR _sourceCategory=${sumologic_http_source.HTTP_Lire_Staging.category} OR _sourceCategory=nyt-kraken-prd OR _collector=k8s.gcp.nyt-kraken-dev.us-east.auth-seshd-local-1"
+  capabilities     = ["viewScheduledViews", "metricsExtraction", "manageMonitors", "viewPartitions", "viewFields", "viewFieldExtraction", "createAccessKeys", "changeDataAccessLevel", "manageCollectors", "viewCollectors"]
 }
 
 resource "ad_group_to_ou" "ad-group-Auth" {
