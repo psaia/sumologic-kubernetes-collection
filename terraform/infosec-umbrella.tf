620ad4df3ba37bde6f49e796c43e81d0995d717d
Shawn Bower <shawn.bower@gmail.com>
2021-02-01 20:58:26 -0500

    Feature/need real corelight role (#189)
    * remove corelight s3 source until we have a valid role
    
    * remove corelight s3 source until we have a valid role

--- terraform/infosec-umbrella.tf
+++ terraform/infosec-umbrella.tf
@@ -1,20 +1,20 @@
-resource "sumologic_s3_source" "infosec-umbrella" {
-  name          = "umbrella Logs"
-  description   = "Log files from umbrella appliances"
-  category      = "aws/s3"
-  content_type  = "AwsS3Bucket"
-  scan_interval = 300000
-  paused        = false
-  collector_id  = module.nytimes-infosec-umbrella.sumologic_collector_id
+// resource "sumologic_s3_source" "infosec-umbrella" {
+//   name          = "umbrella Logs"
+//   description   = "Log files from umbrella appliances"
+//   category      = "aws/s3"
+//   content_type  = "AwsS3Bucket"
+//   scan_interval = 300000
+//   paused        = false
+//   collector_id  = module.nytimes-infosec-umbrella.sumologic_collector_id
 
-  authentication {
-    type     = "AWSRoleBasedAuthentication"
-    role_arn = "arn:aws:iam::147536899606:role/umbrella-sumo-ingest-role"
-  }
+//   authentication {
+//     type     = "AWSRoleBasedAuthentication"
+//     role_arn = "arn:aws:iam::147536899606:role/umbrella-sumo-ingest-role"
+//   }
 
-  path {
-    type            = "S3BucketPathExpression"
-    bucket_name     = "infosec-dev-umbrella-logs"
-    path_expression = "*"
-  }
-}
+//   path {
+//     type            = "S3BucketPathExpression"
+//     bucket_name     = "infosec-dev-umbrella-logs"
+//     path_expression = "*"
+//   }
+// }
