18ac50cd5710cd4cfaedc1ec06f2bf07580ae0ad
Brian Wanner <12836277+BrianWanner@users.noreply.github.com>
2021-02-04 07:24:03 -0500

    update role and bucket name (#192)
    Co-authored-by: BrianWanner <BrianWanner@users.noreply.github.com>

--- terraform/infosec-corelight.tf
+++ terraform/infosec-corelight.tf
@@ -1,20 +1,20 @@
-// resource "sumologic_s3_source" "infosec-corelight" {
-//   name          = "Corelight Logs"
-//   description   = "Log files from Corelight appliances"
-//   category      = "aws/s3"
-//   content_type  = "AwsS3Bucket"
-//   scan_interval = 300000
-//   paused        = false
-//   collector_id  = module.nytimes-infosec-corelight.sumologic_collector_id
+resource "sumologic_s3_source" "infosec-corelight" {
+  name          = "corelight Logs"
+  description   = "Log files from corelight appliances"
+  category      = "aws/s3"
+  content_type  = "AwsS3Bucket"
+  scan_interval = 300000
+  paused        = false
+  collector_id  = module.nytimes-infosec-corelight.sumologic_collector_id
 
-//   authentication {
-//     type     = "AWSRoleBasedAuthentication"
-//     role_arn = "arn:aws:iam::550861604386:role/nyt-sumo-corelight-role"
-//   }
+  authentication {
+    type     = "AWSRoleBasedAuthentication"
+    role_arn = "arn:aws:iam::147536899606:role/corelight-sumo-ingest-role"
+  }
 
-//   path {
-//     type            = "S3BucketPathExpression"
-//     bucket_name     = "infosec-corelight-51347c84"
-//     path_expression = "*"
-//   }
-// }
+  path {
+    type            = "S3BucketPathExpression"
+    bucket_name     = "infosec-corelight-396d6b7c"
+    path_expression = "*"
+  }
+}
